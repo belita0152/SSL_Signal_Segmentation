c479dd8453840d1dd16123801bd86d1583b41ad4
belita0152 <108735429+belita0152@users.noreply.github.com>
2025-03-29 15:35:14 +0900

    Update dataloader.py

--- pretrained/dataloader.py
+++ pretrained/dataloader.py
@@ -1,71 +1,77 @@
 import glob
 import os
+import mne
 import numpy as np
 import pandas as pd
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from sklearn.model_selection import train_test_split
-
-base_path = os.path.join(os.getcwd(), 'shhs2_o')
-data_path = sorted(glob.glob(os.path.join(base_path, '**/*data.parquet')))  # len: 2535
-mask_path = sorted(glob.glob(os.path.join(base_path, '**/*mask.parquet')))  # len: 2535
-data_split_ratio = {'SSL': 0.7, 'Tuning': 0.2, 'Eval': 0.1}
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import torch
 
 
-class SleepDataset(Dataset):
-    def __init__(self, mode):
-        # 전체 데이터셋
-        from parser import Segment
-        total_x, total_y = Segment().parser(data_path, mask_path)
-        self.inputs = total_x
-        self.labels = total_y
+class TorchDataset(Dataset):  # Parsing + ToTensor
+    def __init__(self, transform=None):
+        self.ch_names = ['AIRFLOW', 'THOR RES', 'ABDO RES', 'SaO2']
+        self.event_name = ['Obstructive apnea',  'Central apnea', 'Mixed apnea',  'Hypopnea', 'Arousal', 'SpO2 artifact']
+        self.second = 300  # segmentation size
+        self.sfreq = 10  # sampling rate
 
-          # Split train/tuning/eval dataset
-        tuning_ratio = data_split_ratio['Tuning'] / (data_split_ratio['Tuning'] + data_split_ratio['Eval'])
-        eval_ratio = data_split_ratio['Eval'] / (data_split_ratio['Tuning'] + data_split_ratio['Eval'])
-        
-        x_pretrained, x_finetuned, y_pretrained, y_finetuned = train_test_split(
-            self.inputs, self.labels, train_size=data_split_ratio['SSL'], random_state=777
-        )
-        
-        x_tuning, x_eval, y_tuning, y_eval = train_test_split(
-            x_finetuned, y_finetuned, train_size=tuning_ratio, random_state=777
-        )
+        self.data_path = data_path  # len: 2535
+        self.mask_path = mask_path  # len: 2535
+        self.split = {'SSL': 0.7, 'Tuning': 0.2, 'Eval': 0.1}
 
-        # __init__에서 mode를 이용해 data/label 직접 지정 => 뒤에서 추가로 나눌 필요 없음
-        if mode == 'train':
-            self.inputs = x_pretrained
-            self.labels = y_pretrained
-        elif mode == 'tuning':
-            self.inputs = x_tuning
-            self.labels = y_tuning
-        elif mode == 'eval':
-            self.inputs = x_eval
-            self.labels = y_eval
+        self.data_x, self.data_y = self.parser(self.data_path, self.mask_path)
+
+    def parser(self, data_dir, mask_dir):
+        total_x, total_y = [], []
+        for x_path, y_path in zip(data_dir, mask_dir):
+            x = pd.read_parquet(x_path).T  # ex) (258000, 4) -> (4, 258000)
+            y = pd.read_parquet(y_path).T  # ex) (258000, 1) -> (1, 258000)
+
+            x, y = np.array(x), np.array(y)  # pd.DF -> np.array
+
+            x = StandardScaler().fit_transform(x)  # 데이터 scaling하는 이유: outlier의 영향 최소화 (mne의 robust scaler를 사용할 것.)
+            x = x.reshape(-1, self.second * self.sfreq, len(self.ch_names))
+            y = y.reshape(-1, self.second * self.sfreq, 1)
+            total_x.append(x)
+            total_y.append(y)
+        total_x, total_y = np.concatenate(total_x), np.concatenate(total_y)
+        return total_x, total_y  # 전체 파싱한 데이터셋
+
 
     def __len__(self):
-        return len(self.inputs)  # x의 크기인지, y의 크기인지?
+        return len(self.data_y)
 
     def __getitem__(self, item):  # 샘플마다 데이터 로드
-        x, y = self.inputs[item], self.labels[item]  # 각 sample 데이터와 label 반환
+        x = torch.tensor(self.data_x[item], dtype=torch.float)  # 각 sample 데이터와 label 반환
+        y = torch.tensor(self.data_y[item], dtype=torch.float)
         return x, y
 
 
+
 if __name__ == '__main__':
-    import os
-    import glob
-    import pandas as pd
     base_path = os.path.join(os.getcwd(), '..', 'shhs2_o')
     data_path = sorted(glob.glob(os.path.join(base_path, '**/*data.parquet')))  # len: 2535
     mask_path = sorted(glob.glob(os.path.join(base_path, '**/*mask.parquet')))  # len: 2535
 
-    from parser import Segment
-    parser = Segment()
-    x1, y1 = parser.parser(data_path, mask_path)
-    print(x1.shape)  # (243207, 3000, 4)
-    print(y1.shape)  # (243207, 3000, 4)
+    dataset = TorchDataset(transform=None)  # class 안에서 파싱 함수 사용하였음
+    train_data, tuning_data, eval_data = random_split(dataset,
+                                                      [dataset.split['SSL'],
+                                                       dataset.split['Tuning'],
+                                                       dataset.split['Eval']])
 
-    import torch
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    data = torch.tensor(x1, dtype=torch.float32)
-    train_dataloader = DataLoader(data, batch_size=64, shuffle=True)
+    train_dataloader = DataLoader(train_data, batch_size=64, shuffle=True)
+    tuning_dataloader = DataLoader(tuning_data, batch_size=64, shuffle=True)
+    eval_dataloader = DataLoader(eval_data, batch_size=64, shuffle=True)
+
+    # 전체 x: (243207, 3000, 4), y: (243207, 3000, 1)
+    print("Train: ", len(train_data))  # 170245
+    print("Tuning: ", len(tuning_data))  # 48642
+    print("Eval: ", len(eval_data))  # 24320
+
+
+    print(train_dataloader)
+    print(tuning_dataloader)
+    print(eval_dataloader)
 
