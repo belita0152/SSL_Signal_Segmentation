5f96dff2b619b6cf68fbc3439b2c9314cbc82a0b
belita0152 <108735429+belita0152@users.noreply.github.com>
2025-04-01 14:47:42 +0900

    Update 03_encoder.py

--- pretrained/03_encoder.py
+++ pretrained/03_encoder.py
@@ -8,11 +8,11 @@ from torch.utils.data import Dataset, DataLoader
 import pickle
 
 
-
-class TupleDataset(Dataset):
+# Tuple (train_data) -> x_train, y_train으로 분리
+class TupleDataset(Dataset):  # 일종의 get data
     def __init__(self, tuple_data):
-        self.features = tuple_data[0]  # 첫 번째 요소 (특성)
-        self.labels = tuple_data[1]  # 두 번째 요소 (라벨)
+        self.features = tuple_data[0]  # x data
+        self.labels = tuple_data[1]  # y labels
 
     def __len__(self):
         return len(self.features)
@@ -68,52 +68,19 @@ class VGG(nn.Module):
             nn.Conv1d(512, 512, kernel_size=3, padding=1),
             nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
-            nn.MaxPool1d(kernel_size=2, stride=2)
-        )
-
-        self.enc5 = nn.Sequential(
-            nn.Conv1d(512, 512, kernel_size=3, padding=1),
-            nn.BatchNorm1d(512),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(512, 512, kernel_size=3, padding=1),
-            nn.BatchNorm1d(512),
-            nn.ReLU(inplace=True),
             nn.Conv1d(512, 512, kernel_size=3, padding=1),
             nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=2, stride=2)
         )
-
-        self.flatten = nn.Flatten()
-
 
     def forward(self, x):
-        features = []
-
         x1 = self.enc1(x)
-        features.append(x1)
-        print(x1.shape)
-
         x2 = self.enc2(x1)
-        features.append(x2)
-        print(x2.shape)
-
         x3 = self.enc3(x2)
-        features.append(x3)
-        print(x3.shape)
+        # x4 = self.enc4(x3)
 
-        x4 = self.enc4(x3)
-        features.append(x4)
-        print(x4.shape)
-
-        x5 = self.enc5(x4)
-        features.append(x5)
-        print(x5.shape)
-
-        x6 = self.flatten(x5)
-        print(x6.shape)
-
-        return x6
+        return x3
 
 
 class ResNet(nn.Module):
@@ -121,7 +88,6 @@ class ResNet(nn.Module):
 
 class EfficientNet(nn.Module):
     pass
-
 
 
 # device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
@@ -132,10 +98,14 @@ class EfficientNet(nn.Module):
 #     train_data = pickle.load(f)
 #
 # train_data = copy.deepcopy(train_data)
-
+#
 # train_data = TupleDataset(train_data)
 # train_dataloader = DataLoader(train_data, shuffle=True, batch_size=64, drop_last=True)
-#
+# for data in train_dataloader:
+#     print(data)
+
+
+
 # train_features, train_labels = next(iter(train_dataloader))
 # print(len(train_data))        # 170245
 # print(len(train_dataloader))  # 2660
@@ -151,3 +121,15 @@ class EfficientNet(nn.Module):
 # print(model_output.shape)
 
 
+# train_features, train_labels = next(iter(train_dataloader))
+# train_features = train_features.to(device)
+# model_output = encoder(train_features)
+
+"""
+torch.Size([64, 64, 1500])
+torch.Size([64, 128, 750])
+torch.Size([64, 256, 375])
+torch.Size([64, 512, 187])
+torch.Size([64, 512, 93])
+torch.Size([64, 47616])
+"""
