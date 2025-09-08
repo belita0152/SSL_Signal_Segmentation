48847b46d40db8433554d5faf7c34fbb70dd6658
belita0152 <ggomi0011@gmail.com>
2025-09-08 16:24:02 +0900

    update

--- pretrained/train_segmenter.py
+++ pretrained/train_segmenter.py
@@ -3,25 +3,21 @@ import numpy as np
 import torch
 import random
 import argparse
-import glob
+from typing import Tuple, Dict
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from torch.utils.tensorboard import SummaryWriter
-from dataloader import TorchDataset
 from torch.utils.data import DataLoader
 import torch.nn as nn
 from torch import optim
 from baseline.segmenter import Segmenter
-from baseline.focal_loss import FocalLoss
-from sklearn.metrics import balanced_accuracy_score, f1_score
+from pretrained.data_loader import get_dataset
+from utils.loss import CrossEntropyDiceLoss
+import utils.metric as metric
+import json
 
 warnings.filterwarnings("ignore")
-
-random_seed = 777
-np.random.seed(random_seed)
-torch.manual_seed(random_seed)
-random.seed(random_seed)
 
 device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 ckpt_dir = './checkpoint_VGG_250902'
@@ -31,129 +27,141 @@ writer = SummaryWriter(log_dir=log_dir)
 
 def get_args():
     parser = argparse.ArgumentParser(description='SSL_Signal_Segmentation')
+
     # Dataset
-    parser.add_argument('--sfreq',  default=10, type=int)
-    parser.add_argument('--split_ratio', default={'Train': 0.65, 'Val': 0.05})
+    parser.add_argument('--dataset_name', default='ahi', choices=['heartbeat', 'ahi'])
+    parser.add_argument('--save_path', type=str, default=os.path.join('..', 'result'))
 
     # Train Hyperparameter
-    parser.add_argument('--train_epochs', default=200, type=int)
+    parser.add_argument('--epochs', default=200, type=int)
     parser.add_argument('--learning_rate', default=1e-5, type=float)
-    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--batch_size', default=1024, type=int)
     parser.add_argument('--save_checkpoint', default=True, type=bool)
     parser.add_argument('--n_channels', default=4, type=int)
     parser.add_argument('--n_classes', default=4, type=int)
     parser.add_argument('--weight_decay', default=1e-4, type=float)
     parser.add_argument('--momentum', default=0.999, type=float)
     parser.add_argument('--device', default=torch.device('cuda:1' if torch.cuda.is_available() else 'cpu'))
+    parser.add_argument('--seed', default=42, type=int)
 
     return parser.parse_args()
 
+def set_seed(seed: int = 42):
+    import random
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
 
-def get_focal_loss(x, y):
-    x = x.transpose(0, 2, 1)
-    x = x.reshape(-1, x.shape[2])
-    y = y.reshape(-1)
+def to_device(batch_x: torch.Tensor, device: torch.device) -> torch.Tensor:
+    return batch_x.to(torch.float32, non_blocking=True).to(device)
 
-    loss = FocalLoss().to(args.device)
+class Trainer(object):
+    def __init__(self, args):
+        self.args = args
+        self.device = torch.device(args.device if torch.cuda.is_available() else 'cpu')
 
-    return loss(x, y)
+        # Model
+        self.model: nn.Module = Segmenter(
+            n_cls=args.n_classes,
+            patch_size=(1, 1000)
+        ).to(self.device)
 
+        self.optimizer = optim.AdamW(self.model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+        self.scheduler = optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=args.epochs)
+        self.criterion = CrossEntropyDiceLoss()
+        self.scaler = torch.cuda.amp.GradScaler()
+
+        # Data Loader
+        self.train_loader, self.eval_loader = self._build_dataloaders()
+
+    def _make_input(self, data: Dict[str, torch.Tensor]) -> torch.Tensor:
+        if args.dataset_name == 'heartbeat':
+            x = torch.stack([data['ECG_1'], data['ECG_2']], dim=1)  # (B, 2, T)
+        elif args.dataset_name == 'ahi':
+            x = torch.stack([data['AIRFLOW'], data['THOR RES'], data['ABDO RES'], data["SaO2"]], dim=1)
+        else:
+            raise ValueError(f"Invalid dataset name provided: {args.dataset_name}")
+        return to_device(x, self.device)
+
+    def train_one_epoch(self, epoch: int):
+        self.model.train()
+        for data, target in self.train_loader:
+            self.optimizer.zero_grad()
+
+            x = self._make_input(data)
+            y = target.long().to(self.device)
+            y[y > 0] = 1        # To binary classification -> Arrhythmia Detection
+
+            with torch.cuda.amp.autocast():
+                logits = self.model(x)
+                loss = self.criterion(logits, y)
+
+            self.scaler.scale(loss).backward()
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+
+        self.scheduler.step(epoch=epoch)
+
+    @torch.inference_mode()
+    def eval_one_epoch(self, epoch: int) -> Dict:
+        self.model.eval()
+        preds_all, reals_all = [], []
+
+        for data, target in self.eval_loader:
+            x = self._make_input(data)
+            y = target.long().to(self.device)
+            if args.dataset_name == 'heartbeat':
+                y[y > 0] = 1        # To binary classification
+            elif args.dataset_name == 'ahi':
+                y[(1 <= y) & (y <= 3)] = 1
+                y[y == 4] = 2
+                y[y == 5] = 3
+
+            logits = self.model(x)
+            preds = logits.argmax(dim=1)
+
+            preds_all.append(preds.detach().cpu())
+            reals_all.append(y.detach().cpu())
+
+        y_pred = torch.cat(preds_all, dim=0).reshape(-1).numpy()
+        y_true = torch.cat(reals_all, dim=0).reshape(-1).numpy()
+
+        result = metric.calculate_segmentation_metrics(y_pred, y_true, num_classes=2)
+        accuracy, iou_macro, dice_macro = result['accuracy'], result['iou_macro'], result['dice_macro']
+        print(f'[Epoch]: {epoch:03d} => '
+              f'[Accuracy] : {accuracy*100:.2f} [IoU Macro] : {iou_macro*100:.2f} [Dice Macro] : {dice_macro*100:.2f}')
+        return result
+
+
+    def _build_dataloaders(self) -> Tuple[DataLoader, DataLoader]:
+        train_dataset, eval_dataset = get_dataset(name=self.args.dataset_name)
+        train_loader = DataLoader(
+            dataset=train_dataset,
+            batch_size=self.args.batch_size,
+            shuffle=True,
+        )
+        eval_loader = DataLoader(
+            dataset=eval_dataset,
+            batch_size=self.args.batch_size,
+            shuffle=False,
+            drop_last=False,
+        )
+        return train_loader, eval_loader
+
+    def run(self):
+        results = []
+        for epoch in range(1, self.args.epochs + 1):
+            self.train_one_epoch(epoch)
+            result = self.eval_one_epoch(epoch)
+            results.append(result)
+
+        file_path = os.path.join(args.save_path, args.dataset_name, 'segmenter.json')
+        with open(file_path, 'w', encoding='utf-8') as f:
+            json.dump(results, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
-
-    # 1. Create dataset
     args = get_args()
-    base_path = os.path.join(os.getcwd(), '../../../../data/segmentation/shhs2_o')
-    data_path = sorted(glob.glob(os.path.join(base_path, '**/*data.parquet')))  # len: 2535
-    mask_path = sorted(glob.glob(os.path.join(base_path, '**/*mask.parquet')))  # len: 2535
-
-    # 2. Split into train / tuning / evaluation partitions
-    train_size = int(len(data_path) * args.split_ratio['Train'])
-    train_data_path =  data_path[0:train_size]
-    train_mask_path = mask_path[0:train_size]
-
-    val_size = int(len(data_path) * args.split_ratio['Val'])
-    val_data_path = data_path[train_size:(train_size+val_size)]
-    val_mask_path = mask_path[train_size:(train_size+val_size)]
-
-    train_data = TorchDataset(data_dir=train_data_path, mask_dir=train_mask_path)
-    val_data = TorchDataset(data_dir=val_data_path, mask_dir=val_mask_path)
-
-    # 3. Create dataloaders
-    train_dataloader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, drop_last=True)
-    val_dataloader = DataLoader(val_data, batch_size=args.batch_size, shuffle=False, drop_last=True)
-
-    # 4. Set up
-    model = Segmenter(n_cls=6)
-    model.to(args.device)
-
-    # get_loss = FocalLoss(task_type='multi-label', num_classes=args.n_classes)
-    get_loss = nn.CrossEntropyLoss().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,
-                                                           T_max=len(train_dataloader),
-                                                           eta_min=0,
-                                                           last_epoch=-1)
-    scaler = torch.cuda.amp.GradScaler()
-
-    # 5. Pre-train with Segmenter
-    num_batch = len(train_dataloader)
-    best_f1, best_epoch = 0.0, 0
-    best_model_state, best_score = model.state_dict(), 0
-
-    for epoch in range(args.train_epochs):
-        # (1) Train
-        model.train()
-
-        train_count = 0
-        epoch_train_loss = []
-        for batch, labels in train_dataloader:
-            optimizer.zero_grad()
-
-            with torch.cuda.amp.autocast():
-                x = batch.to(args.device)
-                labels = labels.long().to(args.device)
-                output = model(x)  # output: [16, 6, 3000] / label: [16, 3000]
-
-                loss = get_loss(output, labels)
-                # loss = get_focal_loss(output, labels)
-
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
-
-            train_count += 1
-            epoch_train_loss.append(float(loss.detach().cpu().item()))  # cpu에서 print
-
-        epoch_train_loss = np.mean(np.array(epoch_train_loss))
-
-        with torch.no_grad():
-            preds, labels = [], []
-            for batch, label in val_dataloader:
-                model.eval()
-
-                batch = batch.to(args.device)
-                label = label.long().to(args.device)
-                output = model(batch)  # [16, 6, 3000]
-                output = torch.argmax(output, dim=1)  # [16, 3000]
-                preds.append(output)
-                labels.append(label)
-
-            preds, labels = torch.cat(preds, dim=0), torch.cat(labels, dim=0).squeeze()
-            acc = (preds == labels).float().mean().item()
-
-            preds, labels = preds.detach().cpu().numpy(), labels.detach().cpu().numpy()
-            preds = preds.reshape(-1)
-            labels = labels.reshape(-1)
-
-            val_acc = balanced_accuracy_score(preds, labels)
-            val_mf1 = f1_score(y_true=labels, y_pred=preds, average='macro')
-
-            print('[Epoch] : {0:01d} \t '
-                  '[Train Loss] : {1:.4f} \t '
-                  '[Accuracy] : {2:2.4f}  \t '
-                  '[Macro-F1] : {3:2.4f}'.format(
-                epoch + 1, epoch_train_loss, val_acc * 100, val_mf1 * 100))
-
-    writer.close()
+    set_seed(args.seed)
+    Trainer(args).run()
